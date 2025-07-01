67e2c40c58feeb85ec0580f8af812f6b33f80782
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2025-07-01 14:48:35 +0530

    database schema created

--- src/app.js
+++ src/app.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const connectToDb = require('./config/database');
+const User = require('./models/user');
 const app = express();
 
+app.use(express.json());
+
 // request handler function
-app.use((res, req) => {
-  res.send('Hello from the server');
+app.post('/signup', async (req, res) => {
+  //creating new instance for User model
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.send('User Added Successfully');
+  } catch (error) {
+    res.status(400).send('Error saving the users:' + err.message);
+  }
+});
+
+// Getting user by emailId
+app.get('/user', async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const user = await User.find({ emailId: userEmail });
+    res.send(user);
+  } catch (error) {
+    res.status(400).send('Something went wrong');
+  }
 });
 
 //connecting to database first than listening to port
--- src/config/database.js
+++ src/config/database.js
@@ -1,3 +1,9 @@
 const mongoose = require('mongoose');
 
+const connectToDb = async () => {
+  await mongoose.connect(
+    'mongodb+srv://Testing:lMj3irkla3GSePku@testing.w68coqi.mongodb.net/devTinder'
+  );
+};
+
 module.exports = connectToDb;
